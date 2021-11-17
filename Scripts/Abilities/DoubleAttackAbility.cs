ec900bbd3d190cb110596fd2d3eed31cad1b2d53
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-17 23:33:29 +1300

    Fixed double attack attacking directly in the wrong direction and not double attacking after killing

--- Scripts/Abilities/DoubleAttackAbility.cs
+++ Scripts/Abilities/DoubleAttackAbility.cs
@@ -15,14 +15,16 @@ namespace ZergMod
         public static Ability ability;
 
         private List<int> attackedSlots = new List<int>();
-        
+        private CardSlot directAttackSlot = null; // Gross yes. But i'm lazy
+
         public static void Initialize()
         {
             AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
             info.powerLevel = 0;
             info.rulebookName = "Final Attack";
             info.rulebookDescription = "When this card deals damage it will follow up with one more attack.";
-            info.metaCategories = new List<AbilityMetaCategory> {AbilityMetaCategory.Part1Rulebook, AbilityMetaCategory.Part1Modular};
+            info.metaCategories = new List<AbilityMetaCategory>
+                { AbilityMetaCategory.Part1Rulebook, AbilityMetaCategory.Part1Modular };
 
             List<DialogueEvent.Line> lines = new List<DialogueEvent.Line>();
             DialogueEvent.Line line = new DialogueEvent.Line();
@@ -30,12 +32,24 @@ namespace ZergMod
             lines.Add(line);
             info.abilityLearnedDialogue = new DialogueEvent.LineSet(lines);
 
-            byte[] imgBytes = File.ReadAllBytes(Path.Combine(Plugin.Directory,"Artwork/double_attack.png"));
-            Texture2D tex = new Texture2D(2,2);
+            byte[] imgBytes = File.ReadAllBytes(Path.Combine(Plugin.Directory, "Artwork/double_attack.png"));
+            Texture2D tex = new Texture2D(2, 2);
             tex.LoadImage(imgBytes);
 
-            NewAbility newAbility = new NewAbility(info,typeof(DoubleAttackAbility),tex,AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
+            NewAbility newAbility = new NewAbility(info, typeof(DoubleAttackAbility), tex,
+                AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
             DoubleAttackAbility.ability = newAbility.ability;
+        }
+
+        public override bool RespondsToSlotTargetedForAttack(CardSlot slot, PlayableCard attacker)
+        {
+            return attacker == this.Card;
+        }
+
+        public override IEnumerator OnSlotTargetedForAttack(CardSlot slot, PlayableCard attacker)
+        {
+            directAttackSlot = slot;
+            yield return null;
         }
 
         public override IEnumerator OnAttackEnded()
@@ -51,39 +65,54 @@ namespace ZergMod
 
         public override bool RespondsToDealDamage(int amount, PlayableCard target)
         {
-            return target != null && !target.Dead;
+            return !AlreadyDoubleHitSlot();
         }
 
         public override IEnumerator OnDealDamage(int amount, PlayableCard target)
         {
             yield return base.PreSuccessfulTriggerSequence();
 
-            CardSlot targetSlot = target.slot;
+            CardSlot targetSlot = directAttackSlot;
             int attackingInstanceID = targetSlot.GetInstanceID();
-            if (attackedSlots.Contains(attackingInstanceID))
-            {
-                yield break;
-            }
-
             attackedSlots.Add(attackingInstanceID);
-            yield return Singleton<CombatPhaseManager>.Instance.SlotAttackSlot(Card.Slot, targetSlot, 0.1f);
             
-            yield return base.LearnAbility(0.25f);
-            yield break;
+            yield return new WaitForSeconds(0.4f);
+            if (target.Dead)
+            {
+                // Deal damage directly
+                yield return Singleton<CombatPhaseManager>.Instance.VisualizeCardAttackingDirectly(Card.Slot, targetSlot, amount);
+            }
+            else
+            {
+                // Hit the same card again
+                yield return Singleton<CombatPhaseManager>.Instance.SlotAttackSlot(Card.Slot, targetSlot);
+            }
+            yield return new WaitForSeconds(0.4f);
+
+            yield return base.LearnAbility(0.0f);
+        }
+
+        private bool AlreadyDoubleHitSlot()
+        {
+            CardSlot targetSlot = directAttackSlot;
+            int attackingInstanceID = targetSlot.GetInstanceID();
+            return attackedSlots.Contains(attackingInstanceID);
         }
 
         public override bool RespondsToDealDamageDirectly(int amount)
         {
-            return true;
+            return !AlreadyDoubleHitSlot();
         }
 
         public override IEnumerator OnDealDamageDirectly(int amount)
         {
             yield return base.PreSuccessfulTriggerSequence();
+
+            yield return new WaitForSeconds(0.5f);
+            yield return Singleton<CombatPhaseManager>.Instance.VisualizeCardAttackingDirectly(Card.Slot, directAttackSlot, amount);
+            yield return new WaitForSeconds(0.5f);
             
-            yield return new WaitForSeconds(0.5f);
-            yield return Singleton<CombatPhaseManager>.Instance.VisualizeCardAttackingDirectly(Card.Slot, Card.slot.opposingSlot, amount);
-            yield return new WaitForSeconds(0.5f);
+            yield return base.LearnAbility(0.0f);
         }
     }
 }
