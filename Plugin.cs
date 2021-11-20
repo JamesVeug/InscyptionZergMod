c1c5eef44c76f47524b558e932ee99ca09b04141
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-20 21:17:01 +1300

    Added Dehaka

--- Plugin.cs
+++ Plugin.cs
@@ -62,6 +62,7 @@ namespace ZergMod
             Broodling.Initialize();
             Broodlord.Initialize();
             Drone.Initialize();
+            Dehaka.Initialize();
 
             // Squirrel / Lava
             ChangeSquirrelToLarva();
