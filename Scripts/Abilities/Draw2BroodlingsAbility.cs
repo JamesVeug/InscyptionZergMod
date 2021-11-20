68168b1096fa914661e67ae5e1971276d20f564e
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-21 09:28:51 +1300

    Fixed Draw2Broodlings not appearing in game

--- Scripts/Abilities/Draw2BroodlingsAbility.cs
+++ Scripts/Abilities/Draw2BroodlingsAbility.cs
@@ -26,12 +26,12 @@ namespace ZergMod
 			lines.Add(line);
 			info.abilityLearnedDialogue = new DialogueEvent.LineSet(lines);
 
-			byte[] imgBytes = File.ReadAllBytes(Path.Combine(Plugin.Directory, "Artwork/swam_seeds.png"));
+			byte[] imgBytes = File.ReadAllBytes(Path.Combine(Plugin.Directory, "Artwork/swarm_seeds.png"));
 			Texture2D tex = new Texture2D(2,2);
 			tex.LoadImage(imgBytes);
 
-			NewAbility newAbility = new NewAbility(info,typeof(SpawnLarvaAbility),tex,AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
-			SpawnLarvaAbility.ability = newAbility.ability;
+			NewAbility newAbility = new NewAbility(info,typeof(Draw2BroodlingsAbility),tex,AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
+			Draw2BroodlingsAbility.ability = newAbility.ability;
 		}
 
 		public override bool RespondsToResolveOnBoard()
