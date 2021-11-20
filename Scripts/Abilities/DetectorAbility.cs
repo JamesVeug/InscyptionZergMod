89dde95fabce168b71cd51753bd2a89145d31d93
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-21 09:29:11 +1300

    [WIP] Attempt at fixing Detector. Will need to rethink my approach

--- Scripts/Abilities/DetectorAbility.cs
+++ Scripts/Abilities/DetectorAbility.cs
@@ -37,96 +37,86 @@ namespace ZergMod
             Texture2D tex = new Texture2D(2,2);
             tex.LoadImage(imgBytes);
 
-            NewAbility newAbility = new NewAbility(info,typeof(HealAbility),tex,AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
-            HealAbility.ability = newAbility.ability;
+            NewAbility newAbility = new NewAbility(info,typeof(DetectorAbility),tex,AbilityIdentifier.GetAbilityIdentifier(Plugin.PluginGuid, info.rulebookName));
+            DetectorAbility.ability = newAbility.ability;
+        }
+
+        public override bool RespondsToResolveOnBoard()
+        {
+            return Singleton<TurnManager>.Instance.IsPlayerTurn && GetValidCardSlotsToFlip(false).Count > 0;
+        }
+
+        public override IEnumerator OnResolveOnBoard()
+        {
+            // WHY DOES THIS BRRRRRRREEEEEEEEEAAAAAAAAAAAAAK
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnResolveOnBoard: player: " + Singleton<TurnManager>.Instance.IsPlayerTurn + " ==========");
+            yield return OnUpkeep(Singleton<TurnManager>.Instance.IsPlayerTurn);
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnResolveOnBoard done");
+            yield break;
         }
 
         public override bool RespondsToUpkeep(bool playerUpkeep)
         {
-            bool cardOwnedByPlayer = base.Card.OpponentCard == playerUpkeep; 
-            if(!cardOwnedByPlayer)
-            {
-                // Only do something if its not their turn
-                // Submerge flips all cards owned by whose turn it is
-                return false;
-            }
-
-            List<CardSlot> opposingSlots = base.Card.GetOpposingSlots();
-            for (int i = 0; i < opposingSlots.Count; i++)
-            {
-                CardSlot slot = opposingSlots[i];
-                if (slot.Card != null)
-                {
-                    if (slot.Card.HasAbility(Ability.Submerge) || slot.Card.HasAbility(Ability.SubmergeSquid))
-                    {
-                        // Our targeted
-                        return true;
-                    }
-                }
-            }
-
-            return false;
+            Plugin.Log.LogInfo("[DetectorAbility] ========== RespondsToUpkeep: player: " + playerUpkeep + " ==========");
+            return playerUpkeep && GetValidCardSlotsToFlip(false).Count > 0;
         }
 
         public override IEnumerator OnUpkeep(bool playerUpkeep)
         {
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnUpkeep: player: " + playerUpkeep + " ==========");
             activated = true;
             
             Singleton<ViewManager>.Instance.SwitchToView(View.Board, false, true);
             yield return new WaitForSeconds(0.15f);
             yield return base.PreSuccessfulTriggerSequence();
-            
-            List<CardSlot> opposingSlots = base.Card.GetOpposingSlots();
+
+            List<CardSlot> opposingSlots = GetValidCardSlotsToFlip(false);
             for (int i = 0; i < opposingSlots.Count; i++)
             {
-                CardSlot slot = opposingSlots[i];
-                if (slot.Card != null && !slot.Card.Dead)
-                {
-                    if (slot.Card.HasAbility(Ability.Submerge) || slot.Card.HasAbility(Ability.SubmergeSquid))
-                    {
-                        // Our targeted
-                        base.Card.SetFaceDown(false, false);
-                        base.Card.UpdateFaceUpOnBoardEffects();
-                        yield return new WaitForSeconds(0.3f);
-                    }
-                }
+                PlayableCard card = opposingSlots[i].Card;
+                Plugin.Log.LogInfo("[DetectorAbility] Flipping up " + card.Info.displayedName);
+                yield return base.LearnAbility(0f);
+                        
+                // Our targeted
+                card.SetFaceDown(false, false);
+                card.UpdateFaceUpOnBoardEffects();
+                yield return new WaitForSeconds(0.3f);
             }
             
-            yield return base.LearnAbility(0f);
             this.triggerPriority = int.MinValue + 1;
+            yield return new WaitForSeconds(0.3f); // Because its happening too fast maybe?
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnUpkeep done ==========");
         }
 
         public override bool RespondsToTurnEnd(bool playerTurnEnd)
         {
-            // Only do something if the card that played was owned by us
-            bool cardOwnedByPlayer = base.Card.OpponentCard == playerTurnEnd; 
-            return cardOwnedByPlayer && !base.Card.Dead;
+            // Only do something if the card that played was owned by us 
+            return playerTurnEnd && GetValidCardSlotsToFlip(true).Count > 0;
         }
 
         public override IEnumerator OnTurnEnd(bool playerTurnEnd)
         {
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnTurnEnd: player: " + playerTurnEnd + " ==========");
             activated = false;
             Singleton<ViewManager>.Instance.SwitchToView(View.Board, false, true);
             yield return new WaitForSeconds(0.15f);
             
-            List<CardSlot> opposingSlots = base.Card.GetOpposingSlots();
+            List<CardSlot> opposingSlots = GetValidCardSlotsToFlip(true);
             for (int i = 0; i < opposingSlots.Count; i++)
             {
-                CardSlot slot = opposingSlots[i];
-                if (slot.Card != null && !slot.Card.Dead)
-                {
-                    if (slot.Card.HasAbility(Ability.Submerge) || slot.Card.HasAbility(Ability.SubmergeSquid))
-                    {
-                        // Our targeted
-                        base.Card.SetCardbackSubmerged();
-                        base.Card.SetFaceDown(true, false);
-                        yield return new WaitForSeconds(0.3f);
-                    }
-                }
+                PlayableCard card = opposingSlots[i].Card;
+                Plugin.Log.LogInfo("[DetectorAbility] Flipping down " + card.Info.displayedName);
+                yield return base.LearnAbility(0f);
+                        
+                // Our targeted
+                card.SetCardbackSubmerged();
+                card.SetFaceDown(true, false);
+                yield return new WaitForSeconds(0.3f);
             }
             
-            yield return new WaitForSeconds(0.3f);
             this.triggerPriority = int.MaxValue;
+            yield return new WaitForSeconds(0.3f); // Because its happening too fast maybe?
+            Plugin.Log.LogInfo("[DetectorAbility] ========== OnTurnEnd done ==========");
         }
 
         public override bool RespondsToDie(bool wasSacrifice, PlayableCard killer)
@@ -139,6 +129,31 @@ namespace ZergMod
             // TODO: Card killed while enemy is playing
             // TODO: card is killed while player is playing
             yield return OnTurnEnd(false);
+        }
+
+        private List<CardSlot> GetOpponentSlots(bool isPlayer)
+        {
+            return isPlayer ? BoardManager.Instance.opponentSlots : BoardManager.Instance.playerSlots;
+        }
+
+        private List<CardSlot> GetValidCardSlotsToFlip(bool facingDown)
+        {
+            List<CardSlot> slots = new List<CardSlot>();
+            
+            List<CardSlot> opposingSlots = GetOpponentSlots(Singleton<TurnManager>.Instance.IsPlayerTurn);
+            for (int i = 0; i < opposingSlots.Count; i++)
+            {
+                CardSlot slot = opposingSlots[i];
+                if (slot.Card != null && !slot.Card.Dead)
+                {
+                    if (slot.Card.HasAbility(Ability.Submerge) || slot.Card.HasAbility(Ability.SubmergeSquid) && (slot.Card.FaceDown == facingDown))
+                    {
+                        slots.Add(slot);
+                    }
+                }
+            }
+
+            return slots;
         }
     }
 }
