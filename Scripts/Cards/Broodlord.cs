483b9630367dc9d99daa4b69a26bb180cf9eec88
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-21 15:40:54 +1300

    Broodlord no longer obtainable from Trader

--- Scripts/Cards/Broodlord.cs
+++ Scripts/Cards/Broodlord.cs
@@ -21,7 +21,6 @@ namespace ZergMod.Scripts.Cards
         {
             List<CardMetaCategory> metaCategories = new List<CardMetaCategory>();
             metaCategories.Add(CardMetaCategory.ChoiceNode);
-            metaCategories.Add(CardMetaCategory.TraderOffer);
             metaCategories.Add(CardMetaCategory.Rare);
 
             List<CardAppearanceBehaviour.Appearance> appearanceBehaviour = new List<CardAppearanceBehaviour.Appearance>();
