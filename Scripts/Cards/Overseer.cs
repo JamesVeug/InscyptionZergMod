cfad201ec1fae8b1fa17da1445d95ed11d8ce501
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-21 14:57:52 +1300

    Overseer only obtainable from trader

--- Scripts/Cards/Overseer.cs
+++ Scripts/Cards/Overseer.cs
@@ -20,7 +20,6 @@ namespace ZergMod.Scripts.Cards
         public static void Initialize()
         {
             List<CardMetaCategory> metaCategories = new List<CardMetaCategory>();
-            metaCategories.Add(CardMetaCategory.ChoiceNode);
             metaCategories.Add(CardMetaCategory.TraderOffer);
             metaCategories.Add(CardMetaCategory.Rare);
 
