0ed0b9801ff7596d3a03618b0a9d6ade019cfc97
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-25 23:24:11 +1300

    Draw Locust's - Reduce cards drawn from 2 to 1

--- Scripts/Abilities/Draw2LocustsAbility.cs
+++ Scripts/Abilities/Draw2LocustsAbility.cs
@@ -17,7 +17,7 @@ namespace ZergMod
 			AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
 			info.powerLevel = 0;
 			info.rulebookName = "Draw Locust's";
-			info.rulebookDescription = "Draw 2 Locust's at the start of your turn\nA Locust is defined as: 1 Power 1 Health";
+			info.rulebookDescription = "Draw 1 Locust at the start of your turn\nA Locust is defined as: 1 Power 1 Health";
 			info.metaCategories = new List<AbilityMetaCategory> {AbilityMetaCategory.Part1Rulebook, AbilityMetaCategory.Part1Modular};
 
 			List<DialogueEvent.Line> lines = new List<DialogueEvent.Line>();
@@ -57,7 +57,6 @@ namespace ZergMod
 
 			CardInfo cardByName = CardLoader.GetCardByName("Locust");
 			yield return Singleton<CardSpawner>.Instance.SpawnCardToHand(cardByName, null, 0.25f, null);
-			yield return Singleton<CardSpawner>.Instance.SpawnCardToHand(cardByName, null, 0.125f, null);
 			yield return new WaitForSeconds(0.45f);
 			yield return base.LearnAbility(0.1f);
 			yield break;
