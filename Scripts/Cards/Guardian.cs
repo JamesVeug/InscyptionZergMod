88b75fce50badf785e76de4e229d5fc5d54b830e
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-25 23:33:48 +1300

    Guardian - Now Rare

--- Scripts/Cards/Guardian.cs
+++ Scripts/Cards/Guardian.cs
@@ -24,6 +24,7 @@ namespace ZergMod.Scripts.Cards
             metaCategories.Add(CardMetaCategory.TraderOffer);
 
             List<CardAppearanceBehaviour.Appearance> appearanceBehaviour = new List<CardAppearanceBehaviour.Appearance>();
+            appearanceBehaviour.Add(CardAppearanceBehaviour.Appearance.RareCardBackground);
 
             Texture2D tex = Utils.GetTextureFromPath(TextureFile);
 
