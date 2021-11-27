0bd08ab594a3267cbb45bfb0c69a97dfa0a7e89c
jamesveug <james.veugelaers@outlook.co.nz>
2021-11-27 15:26:37 +1300

    Added custom save data

---
+++ Scripts/Patches/SaveManager_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ZergMod.Patches
+{
+    [HarmonyPatch(typeof (SaveManager), "SaveToFile", new System.Type[] {typeof (bool)})]
+    public class SaveManager_SaveToFile
+    {
+        public static void Postfix()
+        {
+            float secondsSinceLastSave = Time.time - SaveManager.lastSaveTime;
+            if (secondsSinceLastSave < 1)
+            {
+                CustomSaveManager.SaveToFile();
+            }
+            else
+            {
+                Plugin.Log.LogInfo("[SaveManager_SaveToFile] Did not save CustomSave data. Save must have failed. " + secondsSinceLastSave);
+            }
+        }
+    }
+}
