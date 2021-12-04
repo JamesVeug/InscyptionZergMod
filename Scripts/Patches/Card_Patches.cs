02f777e2b2bff0d3eb8784a1ad3250aaf2cd91d5
jamesveug <james.veugelaers@outlook.co.nz>
2021-12-04 22:44:53 +1300

    Added Xel'navga artifact piece 1

--- Scripts/Patches/Card_Patches.cs
+++ Scripts/Patches/Card_Patches.cs
@@ -39,4 +39,33 @@ namespace ZergMod.Patches
             return false;
         }
     }
+    
+    [HarmonyPatch(typeof (Card), "ApplyAppearanceBehaviours", new System.Type[] {typeof (List<CardAppearanceBehaviour.Appearance>)})]
+    public class Card_ApplyAppearanceBehaviours
+    {
+        public static bool Prefix(List<CardAppearanceBehaviour.Appearance> appearances, Card __instance)
+        {
+            foreach (CardAppearanceBehaviour.Appearance appearance in appearances)
+            {
+                if (NewBackgroundBehaviour.Behaviours.TryGetValue(appearance, out NewBackgroundBehaviour behaviour))
+                {
+                    Type type = behaviour.Behaviour;
+                    if (!__instance.gameObject.GetComponent(type))
+                    {
+                        (__instance.gameObject.AddComponent(type) as CardAppearanceBehaviour).ApplyAppearance();
+                    }
+                }
+                else
+                {
+                    Type type = CustomType.GetType("DiskCardGame", appearance.ToString());
+                    if (!__instance.gameObject.GetComponent(type))
+                    {
+                        (__instance.gameObject.AddComponent(type) as CardAppearanceBehaviour).ApplyAppearance();
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
 }
