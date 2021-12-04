fd89c7695f12dbcd702d727282bb8708c8935c4f
jamesveug <james.veugelaers@outlook.co.nz>
2021-12-04 23:53:50 +1300

    Dehaka - Increased blood cost from 1 to 2

--- Scripts/Cards/Dehaka.cs
+++ Scripts/Cards/Dehaka.cs
@@ -15,7 +15,7 @@ namespace ZergMod.Scripts.Cards
 
         private const int BaseAttack = 1;
         private const int BaseHealth = 2;
-        private const int BloodCost = 1;
+        private const int BloodCost = 2;
         private const int BoneCost = 0;
 
         public static void Initialize()
