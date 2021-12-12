04d4c6c9c1d98b016fafd20dd01edbd89b3fbac9
jamesveug <james.veugelaers@outlook.co.nz>
2021-12-12 22:17:28 +1300

    BloodBank - no longer gives you a Broodling

--- Scripts/Abilities/BloodBankAbility.cs
+++ Scripts/Abilities/BloodBankAbility.cs
@@ -43,43 +43,5 @@ namespace ZergMod
 			);
 			BloodBankAbility.ability = newAbility.ability;
 		}
-
-		public override bool RespondsToSacrifice()
-		{
-			return !Card.Dead;
-		}
-
-		public override IEnumerator OnSacrifice()
-		{
-			return base.OnSacrifice();
-		}
-
-		public override bool RespondsToTurnEnd(bool playerTurnEnd)
-		{
-			return !Card.Dead && !playerTurnEnd && Card.slot.IsPlayerSlot;
-		}
-
-		public override IEnumerator OnTurnEnd(bool playerTurnEnd)
-		{
-			yield return PreSuccessfulTriggerSequence();
-			yield return CreateCards();
-			yield return LearnAbility(0f);
-		}
-
-		private IEnumerator CreateCards()
-		{
-			if (Singleton<ViewManager>.Instance.CurrentView != View.Default)
-			{
-				yield return new WaitForSeconds(0.2f);
-				Singleton<ViewManager>.Instance.SwitchToView(View.Default, false, false);
-				yield return new WaitForSeconds(0.2f);
-			}
-
-			CardInfo cardByName = CardLoader.GetCardByName("Broodling");
-			yield return Singleton<CardSpawner>.Instance.SpawnCardToHand(cardByName, null, 0.25f, null);
-			yield return new WaitForSeconds(0.45f);
-			yield return base.LearnAbility(0.1f);
-			yield break;
-		}
 	}
 }
