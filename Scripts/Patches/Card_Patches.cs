afcb4230ecdce702a473de35db4ccfc93cbcd586
jamesveug <james.veugelaers@outlook.co.nz>
2021-12-19 20:51:23 +1300

    Removed patch for NewBackgrounds - in API now

--- Scripts/Patches/Card_Patches.cs
+++ Scripts/Patches/Card_Patches.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using APIPlugin;
+﻿using APIPlugin;
 using DiskCardGame;
 using HarmonyLib;
 
@@ -52,35 +50,6 @@ namespace ZergMod.Scripts.Patches
                     portraitChanges.RefreshPortrait();
                 }
             }
-        }
-    }
-    
-    [HarmonyPatch(typeof (Card), "ApplyAppearanceBehaviours", new System.Type[] {typeof (List<CardAppearanceBehaviour.Appearance>)})]
-    public class Card_ApplyAppearanceBehaviours
-    {
-        public static bool Prefix(List<CardAppearanceBehaviour.Appearance> appearances, Card __instance)
-        {
-            foreach (CardAppearanceBehaviour.Appearance appearance in appearances)
-            {
-                if (NewBackgroundBehaviour.Behaviours.TryGetValue(appearance, out NewBackgroundBehaviour behaviour))
-                {
-                    Type type = behaviour.Behaviour;
-                    if (!__instance.gameObject.GetComponent(type))
-                    {
-                        (__instance.gameObject.AddComponent(type) as CardAppearanceBehaviour).ApplyAppearance();
-                    }
-                }
-                else
-                {
-                    Type type = CustomType.GetType("DiskCardGame", appearance.ToString());
-                    if (!__instance.gameObject.GetComponent(type))
-                    {
-                        (__instance.gameObject.AddComponent(type) as CardAppearanceBehaviour).ApplyAppearance();
-                    }
-                }
-            }
-
-            return false;
         }
     }
 }
