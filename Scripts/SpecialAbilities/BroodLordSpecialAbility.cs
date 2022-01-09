dde5c0cfa4c0cd0d998365ae8c68a361056bde09
jamesveug <james.veugelaers@outlook.co.nz>
2022-01-09 19:16:18 +1300

    Broodlord - Removed unnessesary check

--- Scripts/SpecialAbilities/BroodLordSpecialAbility.cs
+++ Scripts/SpecialAbilities/BroodLordSpecialAbility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using DiskCardGame;
 using UnityEngine;
 using ZergMod.Scripts.Data.Sigils;
@@ -11,8 +12,6 @@ namespace ZergMod.Scripts.SpecialAbilities
         public SpecialTriggeredAbility SpecialAbility => specialAbility;
         public static SpecialTriggeredAbility specialAbility = SpecialTriggeredAbility.None;
 
-        private bool triggered = false;
-        
         public static void Initialize(Type declaringType)
         {
             specialAbility = InitializeBase(declaringType);
@@ -20,13 +19,11 @@ namespace ZergMod.Scripts.SpecialAbilities
 
         public override bool RespondsToUpkeep(bool playerUpkeep)
         {
-            return !triggered && !PlayableCard.Dead && playerUpkeep && PlayableCard.slot.IsPlayerSlot;
+            return !PlayableCard.Dead && playerUpkeep && PlayableCard.slot.IsPlayerSlot;
         }
 
         public override IEnumerator OnUpkeep(bool playerUpkeep)
         {
-            triggered = true; // Because this triggers twice for some reason. 
-            
             View currentView = Singleton<ViewManager>.Instance.CurrentView;
             if (currentView != View.Hand)
             {
@@ -45,17 +42,6 @@ namespace ZergMod.Scripts.SpecialAbilities
                 Singleton<ViewManager>.Instance.SwitchToView(currentView, false, false);
                 yield return new WaitForSeconds(0.2f);
             }
-        }
-
-        public override bool RespondsToTurnEnd(bool playerTurnEnd)
-        {
-            return triggered;
-        }
-
-        public override IEnumerator OnTurnEnd(bool playerTurnEnd)
-        {
-            triggered = false;
-            yield return null;
         }
     }
 }
