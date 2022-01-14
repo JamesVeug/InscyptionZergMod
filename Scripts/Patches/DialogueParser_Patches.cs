3737d208f8fefc9e7cc27254b6bc14d1a57a3d90
jamesveug <james.veugelaers@outlook.co.nz>
2022-01-14 22:27:04 +1300

    Added Purple color for text

---
+++ Scripts/Patches/DialogueParser_Patches.cs
@@ -0,0 +1,25 @@
+﻿using APIPlugin;
+using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ZergMod.Scripts.Patches
+{
+    [HarmonyPatch(typeof (DialogueParser), "GetColorFromCode", new System.Type[] {typeof (string), typeof(Color)})]
+    public class DialogueParser_GetColorFromCode
+    {
+        public static bool Prefix(DialogueParser __instance, string code, Color defaultColor, ref Color __result)
+        {
+            string stringValue = DialogueParser.GetStringValue(code, "c");
+            Plugin.Log.LogInfo($"stringValue: '{stringValue}'");
+            switch (stringValue)
+            {
+                case "purple":
+                    __result = new Color(0.3f, 0, 0.7f);
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
