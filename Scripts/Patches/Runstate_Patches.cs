f0c22898c62b5cfa5f1c893661ebf8f03c7beb1d
jamesveug <james.veugelaers@outlook.co.nz>
2022-01-16 23:20:58 +1300

    Dehaka - Kills resets for run now

---
+++ Scripts/Patches/Runstate_Patches.cs
@@ -0,0 +1,16 @@
+﻿using APIPlugin;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace ZergMod.Scripts.Patches
+{
+    [HarmonyPatch(typeof(RunState), "Initialize")]
+    public class RunState_Initialize
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            CustomSaveManager.SaveFile.DehakaKills = 0;
+        }
+    }
+}
