96054841ee688ec8a8d12be6d474022bf68215fb
jamesveug <james.veugelaers@outlook.co.nz>
2022-03-20 20:53:32 +1300

    Removed more deathcard stuff

--- Scripts/Patches/Utils.cs
+++ Scripts/Patches/Utils.cs
@@ -8,19 +8,6 @@ namespace ZergMod.Scripts.Patches
 {
     public static class Utils
     {
-        public static T AttachMonoBehaviour<T>(Type type, GameObject gameObject)
-        {
-            SpecialCardBehaviour t = gameObject.GetComponent(type) as SpecialCardBehaviour;
-            if (t == null)
-            {
-                return (T)(object)gameObject.AddComponent(type);
-            }
-            else
-            {
-                return (T)(object)t;
-            }
-        }
-        
         public static bool OpponentHasADetector(PlayableCard card)
         {
             List<CardSlot> opponentSlots = Singleton<BoardManager>.Instance.GetSlots(card.OpponentCard);
